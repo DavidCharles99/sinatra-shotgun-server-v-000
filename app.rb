23fd2b75ce73f401d0f43bd6754117e8afa72491
David Charles <ratlover03@gmail.com>
2019-09-25 19:52:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app mother fuckers!!!! "
+    "Welcome to your app mother fuckers!!!! \n This is pretty neat "
   end
 
 end
